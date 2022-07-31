a2a039011986390964cde7bd64a34f2768baa91f
Piyush-Narsinghani <74352265+Piyush-Narsinghani@users.noreply.github.com>
2022-07-31 12:42:00 +0530

    upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>This is an About Component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint alias inventore aut aperiam odio! Facere quos omnis quod vitae dignissimos error quam eum excepturi voluptate minima, placeat tempora. Delectus odio repellat perferendis qui sunt.
+            
+        </p>
+    </div>
+  )
+}
